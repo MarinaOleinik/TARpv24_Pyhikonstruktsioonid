c4bc07607a6e1351fa2e24eea51d12fa22b8fb5a
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2025-01-20 11:27:16 +0200

    print(), input() funktsioonid 20.01.25

--- TARpv24_Pyhikonstruktsioonid.py
+++ TARpv24_Pyhikonstruktsioonid.py
@@ -1,2 +1,21 @@
 print("Tere tulemast! 1","Tere tulemast! 2",end="...",sep="---") #selgitus
 #kommentaar
+# andmetüübid 
+# täisarv- int, ujukomaarv - float, sõne- str, tõevärtused - bool
+#astendamine **
+print()
+a=5
+aste=2
+tulemus=a**aste
+print(a,"**",aste,"=",tulemus)
+#korrutamine *
+# arv1=int(input("Esimene korrutaja: "))
+# arv2=int(input("Teine korrutaja: "))
+# tulemus=arv1*arv2
+# print(arv1,"*",arv2,"=",tulemus)
+#jagamine /
+arv1=int(input("Esimene arv: "))
+arv2=int(input("Jagaja: "))
+tulemus=arv1/arv2
+print(arv1,"/",arv2,"=",tulemus)
+print(type(tulemus))
